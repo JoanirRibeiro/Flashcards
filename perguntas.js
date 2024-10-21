c2b782b1239523b80417d2a07b1a3ec6cad16c0f
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-10-21 12:36:14 +0000

    corrigindo problemas de semantica no perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criarCartao(
     "Programação",
     "O que é JavaScript?",
-    "O JavaScript é uma linguagem de programação".
+    "O JavaScript é uma linguagem de programação"
 )
 
 criarCartao(
     "Programação",
     "O que é CSS?",
-    "O CSS é uma lingaugem de estilização".
+    "O CSS é uma lingaugem de estilização"
 )
 
 criarCartao(
     "Programação",
     "O que é HTML?",
-    "HTML (Linguagem de Marcação de Hipertexto) é o código que você usa para estruturar uma página web e seu conteúdo".
+    "HTML (Linguagem de Marcação de Hipertexto) é o código que você usa para estruturar uma página web e seu conteúdo"
 )
 
 criarCartao(
     "Programação",
-    "O que é Web?"
-    "A web desiga a rede que conecta computadores por todo mundo a World Wide Web WWW".
+    "O que é Web?",
+    "A web desiga a rede que conecta computadores por todo mundo, a World Wide Web (WWW)"
 )
