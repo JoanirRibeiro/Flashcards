6e99a828db330437ceadafb5eb7d7958ae2f0fb9
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-11-27 12:02:25 +0000

    inserindo imagens

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criarCartao(categoria, pergunta, resposta){
+function criarCartao(categoria, pergunta, resposta, imagem){
     let cartao = document.createElement('article')
     cartao.className = 'cartao';
     cartao.innerHTML = `
@@ -6,6 +6,7 @@ function criarCartao(categoria, pergunta, resposta){
                 <h3>${categoria}</h3>
                 <div class="cartao__conteudo__pergunta">
                     <p>${pergunta}</p>
+                    <img src="assets/img/${imagem}">
                 </div>
                 <div class="cartao__conteudo__resposta">
                     <p>${resposta}</p>
--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,27 @@
 criarCartao(
     "Programação",
     "O que é JavaScript?",
-    "O JavaScript é uma linguagem de programação"
+    "O JavaScript é uma linguagem de programação",
+    "front-end.png"
 )
 
 criarCartao(
     "Programação",
     "O que é CSS?",
-    "O CSS é uma lingaugem de estilização"
+    "O CSS é uma lingaugem de estilização",
+    "front-end.png"
 )
 
 criarCartao(
     "Programação",
     "O que é HTML?",
-    "HTML (Linguagem de Marcação de Hipertexto) é o código que você usa para estruturar uma página web e seu conteúdo"
+    "HTML (Linguagem de Marcação de Hipertexto) é o código que você usa para estruturar uma página web e seu conteúdo",
+    "front-end.png"
 )
 
 criarCartao(
     "Programação",
     "O que é Web?",
-    "A web desiga a rede que conecta computadores por todo mundo, a World Wide Web (WWW)"
+    "A web desiga a rede que conecta computadores por todo mundo, a World Wide Web (WWW)",
+    "front-end.png"
 )
